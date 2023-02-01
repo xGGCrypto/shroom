7d1798b20b4e0a9853088342888aa61b53961ccc
xGGCrypto <83046057+xGGCrypto@users.noreply.github.com>
2023-01-31 22:26:50 -0500

    FurnitureHelper - getFurniOptions

--- src/objects/furniture/FurnitureHelper.ts
+++ src/objects/furniture/FurnitureHelper.ts
@@ -23,4 +23,41 @@ export class FurnitureHelper {
             });
         });
     }
+
+    public static async getFurniOptions(furni: IFurniture, shroom: Shroom): Promise<FurniOptions | undefined> {
+        if (!furni.type) return; // If Furniture Type == Undefined, Return Undefined
+        const info = await shroom.dependencies.furnitureData?.getInfo(furni.type);
+        if (!info) return; // If FurnitureData Fetch Fails, Return Undefined
+    
+        const image = (await this.getFurniPreview(furni.type, shroom)).src;
+        const validDirections = await furni.validDirections;
+    
+        return {
+          name: info?.name,
+          type: furni.type,
+          description: info?.description,
+          image,
+    
+          move: (callback) => { callback(); },
+          rotate: () => {
+            const currIndex = validDirections.indexOf(furni.direction);
+            var nextDirection = 0;
+    
+            if (currIndex + 1 > validDirections.length) { nextDirection = validDirections[0]; }
+            else { nextDirection = validDirections[currIndex + 1]; }
+            furni.direction = nextDirection;
+          },
+          pickup: (callback) => { callback(); },
+        };
+    }
+}
+
+interface FurniOptions {
+    name?: string;
+    type: string;
+    description?: string;
+    image: string;
+    move: (callback: () => void) => void;
+    rotate: () => void;
+    pickup: (callback: () => void) => void;
 }
