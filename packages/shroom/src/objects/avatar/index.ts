6d38303598bd5db85d9633d585480777c31b1b07
xGGCrypto <xggcrypto@gmail.com>
2025-08-08 01:39:01 -0400

    Fix Export

--- packages/shroom/src/objects/avatar/index.ts
+++ packages/shroom/src/objects/avatar/index.ts
@@ -1,4 +1,4 @@
 export { Avatar } from "./Avatar";
-export type { BaseAvatarDependencies, BaseAvatarOptions } from "./BaseAvatar";
+export type { BaseAvatarDependencies, BaseAvatarOptions } from "./types";
 export { BaseAvatar } from "./BaseAvatar";
 export { AvatarLoader } from "./AvatarLoader";
